c3f313ffd9cfe2cc85e116c2ca9311b9fbc2b066
GauravPratap1988ster <115794844+GauravPratap1988ster@users.noreply.github.com>
2022-11-22 16:58:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from verify_num import verify_number_from_colors
-from verify_color import verify_colors_from_number
+from verify_num import verify_num
+from verify_color import verify_color
 from colour_referenceM import print_manual
 
 if __name__ == '__main__':
-  verify_number_from_colors(4, 'White', 'Brown')
-  verify_number_from_colors(5, 'White', 'Slate')
-  verify_colors_from_number('Black', 'Orange', 12)
-  verify_colors_from_number('Violet', 'Slate', 25)
-  verify_colors_from_number('Red', 'Orange', 7)
+  verify_num(4, 'White', 'Brown')
+  verify_num(5, 'White', 'Slate')
+  verify_color('Black', 'Orange', 12)
+  verify_color('Violet', 'Slate', 25)
+  verify_color('Red', 'Orange', 7)
   print_manual()
   print('Done :)')
