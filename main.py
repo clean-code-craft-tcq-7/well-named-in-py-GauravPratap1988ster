3de1bb58bcf2a1daaed4817de0f45b80dce9b630
GauravPratap1988ster <115794844+GauravPratap1988ster@users.noreply.github.com>
2022-11-22 15:00:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from verify_colors_using_number import verify_colors_from_number
 from colour_coding_reference_manual import print_manual
 
 if __name__ == '__main__':
-  verify_number_from_colors(4, 'White', 'Brown')
-  verify_number_from_colors(5, 'White', 'Slate')
-  verify_colors_from_number('Black', 'Orange', 12)
-  verify_colors_from_number('Violet', 'Slate', 25)
-  verify_colors_from_number('Red', 'Orange', 7)
+  verify_num(4, 'White', 'Brown')
+  verify_num(5, 'White', 'Slate')
+  verify_color('Black', 'Orange', 12)
+  verify_color('Violet', 'Slate', 25)
+  verify_color('Red', 'Orange', 7)
   print_manual()
   print('Done :)')
