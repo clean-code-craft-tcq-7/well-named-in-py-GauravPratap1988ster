a3cb13b9e46e785be5477cf8a8373ab204db87d2
GauravPratap1988ster <115794844+GauravPratap1988ster@users.noreply.github.com>
2022-11-22 14:54:28 +0530

    Update verify_num.py

--- verify_num.py
+++ verify_num.py
@@ -11,6 +11,6 @@ def get_pair_number_from_color(major_color, minor_color):
     raise Exception('Minor index out of range')
   return major_index * len(MINOR_COLORS) + minor_index + 1
 
-def verify_number_from_colors(expected_pair_number, major_color, minor_color):
+def verify_number(expected_pair_number, major_color, minor_color):
   pair_number = get_pair_number_from_color(major_color, minor_color)
   assert pair_number == expected_pair_number, "pair number is not matching with given pair of colors"
