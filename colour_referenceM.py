924109fda6c536ef69fee60fd18bf6b1d55c133b
GauravPratap1988ster <115794844+GauravPratap1988ster@users.noreply.github.com>
2022-11-22 14:57:13 +0530

    Update colour_referenceM.py

--- colour_referenceM.py
+++ colour_referenceM.py
@@ -1,4 +1,4 @@
-from classification_colors import MAJOR_COLORS, MINOR_COLORS
+from classification_colours import MAJOR_COLORS, MINOR_COLORS
 
 def get_color_code_manual():
     color_code_pair_list = []
